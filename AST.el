54d1f2ec797bf4016e906707fa41cc7a8fa94541
Marcos Magueta <maguetamarcos@gmail.com>
2022-06-18 00:51:41 -0300

    feat: add condition expressions

--- AST.el
+++ AST.el
@@ -43,7 +43,13 @@
   (abstraction :type AbstractionT)
   (literal :type LiteralT))
 
+(cl-defstruct ConditionT
+  (condition :type ExpressionT)
+  (then :type ExpressionT)
+  (else :type ExpressionT))
+
 (defun eval-arithmetic (op env left right)
+  ""
   (let ((evaluated-left (eval-ecaml left env))
 	(evaluated-right (eval-ecaml right env)))
     (cond ((and (LiteralT-p evaluated-left) (LiteralT-p evaluated-right))
@@ -57,14 +63,26 @@
     (elt found-variable 1)))
 
 (defun eval-closure (arg body closed-env env value)
+  ""
   (let ((evaluated-value (eval-ecaml value env)))
     (ht-set closed-env arg evaluated-value)
     (eval-ecaml body (ht-merge closed-env env))))
 
 (defun eval-application (f value env)
+  ""
   (let ((exp (eval-ecaml f env)))
     (cl-typecase exp
       (ClosureT (eval-closure (ClosureT-var exp) (ClosureT-expression exp) (ClosureT-environment exp) env value)))))
+
+(defun eval-condition (control-expression env)
+  ""
+  (let ((evaluated-expression (eval-ecaml (ConditionT-condition control-expression) env)))
+    (if (and (LiteralT-p evaluated-expression)
+	     (booleanp (LiteralT-value evaluated-expression)))
+	(if (LiteralT-value evaluated-expression)
+	    (eval-ecaml (ConditionT-then control-expression) env)
+	  (eval-ecaml (ConditionT-else control-expression) env))
+      (error "ConditionT expression failed on evaluation"))))
 
 (defun eval-ecaml (exp &optional env)
   ""
@@ -74,12 +92,15 @@
     (ArithmeticT (eval-arithmetic (ArithmeticT-operation exp) env (ArithmeticT-left exp) (ArithmeticT-right exp)))
     (VariableT (eval-variable (VariableT-label exp) env))
     (AbstractionT (make-ClosureT :var (AbstractionT-param exp) :expression (AbstractionT-body exp) :environment env))
+    (ConditionT (eval-condition exp env))
     (ClosureT exp)
     (ApplicationT (eval-application (ApplicationT-abstraction exp) (ApplicationT-literal exp) env))))
   
 (eval-ecaml (make-LiteralT :value 2))
 
 (eval-ecaml (make-ArithmeticT :operation #'+ :left (make-LiteralT :value 2) :right (make-LiteralT :value 2)))
+
+(eval-ecaml (make-ConditionT :condition (make-LiteralT :value nil) :then (make-LiteralT :value 1) :else (make-LiteralT :value 2)))
 
 (eval-ecaml (make-ApplicationT
  :abstraction (make-AbstractionT
